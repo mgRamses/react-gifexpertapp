0d47ccaddd925f8ebe5f28d341d7ff85edfc9783
Rmasés Mendoza <mg.ramses@gmail.com>
2020-07-09 13:41:21 -0500

    First commmit

---
+++ docs/precache-manifest.68fd11e35945b1c9185a2d856fde550a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88855e2b7100888e536a0e34dc781f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13643e64c93f06bd05cf",
+    "url": "/static/css/main.5733f4cf.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13643e64c93f06bd05cf",
+    "url": "/static/js/main.095f2e22.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
---
+++ src/components/GifGrid.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+import { GiftGridItem } from './GiftGridItem';
+
+export const GifGrid = ({category}) => {
+
+    const {data, loading} = useFetchGifs(category);
+
+    return (
+        <>
+            <h3>{category}</h3>
+
+            {loading && <p>Loading</p>}
+
+            <div className="card-grid">           
+                {
+                    data.map(img => (
+                        <GiftGridItem 
+                            key={img.id}
+                            {...img}/>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
